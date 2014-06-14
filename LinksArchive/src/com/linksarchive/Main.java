9e86d38c5a605e166804ea23ec699dcdf487a8c8
rocasyeh <rocasyeh@gmail.com>
2014-06-15 02:31:21 +0800

    Finish main UI

--- LinksArchive/src/com/linksarchive/Main.java
+++ LinksArchive/src/com/linksarchive/Main.java
@@ -7,11 +7,20 @@ import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
 
 
 public class Main {
 
 	protected Shell shell;
+	private Text txtArchiveSn;
+	private Text txtArchiveName;
+	private Text txtLinks;
 
 	/**
 	 * Launch the application.
@@ -51,10 +60,8 @@ public class Main {
 		shell = new Shell();
 		shell.setSize(535, 426);
 		shell.setText(Messages.Main_0);
-
-		/**
-		 * Create a menu bar 
-		 */
+		FormLayout fl_shell = new FormLayout();
+		shell.setLayout(fl_shell);
 		Menu menuBar = new Menu(shell, SWT.BAR);
 		shell.setMenuBar(menuBar);
 		
@@ -62,77 +69,144 @@ public class Main {
 		 * Create a menu set "File" 
 		 */
 		MenuItem mntmFile = new MenuItem(menuBar, SWT.CASCADE);
-		mntmFile.setText(Messages.Main_txtMntmFile);
+		mntmFile.setText(Messages.Main_MntmFile_text);
 		
 		Menu mnFile = new Menu(mntmFile);
 		mntmFile.setMenu(mnFile);
 		
 		// create a menu item "File->New"
 		MenuItem mntmFile_New = new MenuItem(mnFile, SWT.NONE);
-		mntmFile_New.setText(Messages.Main_txtMntmFile_New);
+		mntmFile_New.setText(Messages.Main_MntmFileNew_text);
 
 		// create a menu item "File->Open..."
 		MenuItem mntmFile_Open = new MenuItem(mnFile, SWT.NONE);
-		mntmFile_Open.setText(Messages.Main_txtMntmFile_Open);
+		mntmFile_Open.setText(Messages.Main_MntmFileOpen_text);
 
 		// create a menu item "File->Save"
 		MenuItem mntmFile_Save = new MenuItem(mnFile, SWT.NONE);
-		mntmFile_Save.setText(Messages.Main_txtMntmFile_Save);
+		mntmFile_Save.setText(Messages.Main_MntmFileSave_text);
 
 		// create a menu item "File->Save and New"
 		MenuItem mntmFile_SaveNew = new MenuItem(mnFile, SWT.NONE);
-		mntmFile_SaveNew.setText(Messages.Main_txtMntmFile_SaveNew);
+		mntmFile_SaveNew.setText(Messages.Main_MntmFileSaveNew_text);
 
 		// create a menu item "File->Save As..."
 		MenuItem mntmFile_SaveAs = new MenuItem(mnFile, SWT.NONE);
-		mntmFile_SaveAs.setText(Messages.Main_txtMntmFile_SaveAs);
+		mntmFile_SaveAs.setText(Messages.Main_MntmFileSaveAs_text);
 
 		// create a menu item "File->Exit"
 		MenuItem mntmFile_Exit = new MenuItem(mnFile, SWT.NONE);
-		mntmFile_Exit.setText(Messages.Main_txtMntmFile_Exit);
+		mntmFile_Exit.setText(Messages.Main_MntmFileExit_text);
 
 		/**
 		 * Create a menu set "Tools" 
 		 */
 		MenuItem mntmTools = new MenuItem(menuBar, SWT.CASCADE);
-		mntmTools.setText(Messages.Main_txtMntmTools);
+		mntmTools.setText(Messages.Main_MntmTools_text);
 		
 		Menu mnTools = new Menu(mntmTools);
 		mntmTools.setMenu(mnTools);
-		
+
+		// create a menu item "Tools->Options"
 		MenuItem mntmTools_Options = new MenuItem(mnTools, SWT.NONE);
-		mntmTools_Options.setText(Messages.Main_txtMntmTools_Option);
+		mntmTools_Options.setText(Messages.Main_MntmToolsOptions_text);
 
 
 		/**
 		 * Create a menu set "Help" 
 		 */
 		MenuItem mntmHelp = new MenuItem(menuBar, SWT.CASCADE);
-		mntmHelp.setText(Messages.Main_txtMntmHelp);
+		mntmHelp.setText(Messages.Main_MntmHelp_text);
 		
 		Menu mnHelp = new Menu(mntmHelp);
 		mntmHelp.setMenu(mnHelp);
-		
+
+		// create a menu item "Help->About"
 		MenuItem mntmHelp_About = new MenuItem(mnHelp, SWT.NONE);
-		mntmHelp_About.setText(Messages.Main_txtMntmHelp_About);
+		mntmHelp_About.setText(Messages.Main_MntmHelpAbout_text);
 		
 		/**
 		 * Create a toolBar for MenuItem's shortcut
 		 */
 		ToolBar toolBar = new ToolBar(shell, SWT.FLAT | SWT.RIGHT);
-		toolBar.setBounds(0, 35, 517, 41);
+		FormData fd_toolBar = new FormData();
+		fd_toolBar.right = new FormAttachment(0, 517);
+		fd_toolBar.top = new FormAttachment(0);
+		fd_toolBar.left = new FormAttachment(0);
+		toolBar.setLayoutData(fd_toolBar);
+
+		// create a tool item "SaveNew"
+		ToolItem tltmSavenew = new ToolItem(toolBar, SWT.NONE);
+		tltmSavenew.setSelection(true);
+		tltmSavenew.setText(Messages.Main_tltmSavenew_text);
+
+		// create a tool item "Save"
+		ToolItem tltmSave = new ToolItem(toolBar, SWT.NONE);
+		tltmSave.setWidth(24);
+		tltmSave.setSelection(true);
+		tltmSave.setText(Messages.Main_tltmSave_text);
+
+		// create a tool item "New"
+		ToolItem tltmNew = new ToolItem(toolBar, SWT.NONE);
+		tltmNew.setWidth(24);
+		tltmNew.setSelection(true);
+		tltmNew.setText(Messages.Main_tltmNew_text);
+
+		/**
+		 * Create Labels and Texts
+		 */
+		// create Archive Serial Number's Label and Text 
+		Label lblArchiveSn = new Label(shell, SWT.NONE);
+		FormData fd_lblArchiveSn = new FormData();
+		fd_lblArchiveSn.top = new FormAttachment(0, 33);
+		fd_lblArchiveSn.left = new FormAttachment(0, 10);
+		fd_lblArchiveSn.right = new FormAttachment(0, 120);
+		lblArchiveSn.setLayoutData(fd_lblArchiveSn);
+		lblArchiveSn.setText(Messages.Main_lblArchiveSn_text);
 		
-		Button btnSaveNew = new Button(shell, SWT.NONE);
-		btnSaveNew.setBounds(10, 72, 94, 29);
-		btnSaveNew.setText(Messages.Main_btnSaveNew_text);
+		txtArchiveSn = new Text(shell, SWT.BORDER);
+		FormData fd_txtArchiveSn = new FormData();
+		fd_txtArchiveSn.top = new FormAttachment(0, 33);
+		fd_txtArchiveSn.left = new FormAttachment(0, 125);
+		fd_txtArchiveSn.right = new FormAttachment(100, -10);
+		txtArchiveSn.setLayoutData(fd_txtArchiveSn);
+		txtArchiveSn.setText("");
+
+		// create Archive Name's Label and Text 
+		Label lblArchiveName = new Label(shell, SWT.NONE);
+		FormData fd_lblArchiveName = new FormData();
+		fd_lblArchiveName.top = new FormAttachment(txtArchiveSn, 10);
+		fd_lblArchiveName.left = new FormAttachment(0, 10);
+		fd_lblArchiveName.right = new FormAttachment(0, 120);
+		lblArchiveName.setLayoutData(fd_lblArchiveName);
+		lblArchiveName.setText(Messages.Main_lblArchiveName_text);
+
+		txtArchiveName = new Text(shell, SWT.BORDER | SWT.WRAP | SWT.V_SCROLL | SWT.MULTI);
+		FormData fd_txtArchiveName = new FormData();
+		fd_txtArchiveName.height = 50;
+		fd_txtArchiveName.top = new FormAttachment(txtArchiveSn, 10);
+		fd_txtArchiveName.right = new FormAttachment(100, -10);
+		fd_txtArchiveName.left = new FormAttachment(0, 125);
+		txtArchiveName.setLayoutData(fd_txtArchiveName);
+		txtArchiveName.setText("");
+
+		// create Links's Label and Text 
+		Label lblLinks = new Label(shell, SWT.NONE);
+		FormData fd_lblLinks = new FormData();
+		fd_lblLinks.top = new FormAttachment(txtArchiveName, 10);
+		fd_lblLinks.left = new FormAttachment(0, 10);
+		fd_lblLinks.right = new FormAttachment(0, 120);
+		lblLinks.setLayoutData(fd_lblLinks);
+		lblLinks.setText(Messages.Main_lblLinks_text);
 		
-		Button btnSave = new Button(shell, SWT.NONE);
-		btnSave.setBounds(91, 0, 94, 29);
-		btnSave.setText(Messages.Main_btnSave_text);
-		
-		Button btnNew = new Button(shell, SWT.NONE);
-		btnNew.setBounds(185, 0, 94, 29);
-		btnNew.setText(Messages.Main_btnNew_text);
+		txtLinks = new Text(shell, SWT.BORDER | SWT.WRAP | SWT.V_SCROLL | SWT.MULTI);
+		FormData fd_txtLinks = new FormData();
+		fd_txtLinks.top = new FormAttachment(txtArchiveName, 10);
+		fd_txtLinks.bottom = new FormAttachment(100, -10);
+		fd_txtLinks.left = new FormAttachment(0, 125);
+		fd_txtLinks.right = new FormAttachment(100, -10);
+		txtLinks.setLayoutData(fd_txtLinks);
+		txtLinks.setText("");
 
 	}
 }
--- LinksArchive/src/com/linksarchive/Messages.java
+++ LinksArchive/src/com/linksarchive/Messages.java
@@ -4,21 +4,26 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 	public static String Main_0;
-	public static String Main_txtMntmFile;
-	public static String Main_txtMntmFile_Exit;
-	public static String Main_txtMntmFile_New;
-	public static String Main_txtMntmFile_Open;
-	public static String Main_txtMntmFile_Save;
-	public static String Main_txtMntmFile_SaveAs;
-	public static String Main_txtMntmFile_SaveNew;
-	public static String Main_txtMntmHelp;
-	public static String Main_txtMntmHelp_About;
-	public static String Main_txtMntmTools;
-	public static String Main_txtMntmTools_Option;
-	public static String Main_btnSavenew_text;
-	public static String Main_btnNewButton_text;
-	public static String Main_btnNewButton_1_text;
-	public static String Main_btnNewButton_text_1;
+	public static String Main_MntmFile_text;
+	public static String Main_MntmFileExit_text;
+	public static String Main_MntmFileNew_text;
+	public static String Main_MntmFileOpen_text;
+	public static String Main_MntmFileSave_text;
+	public static String Main_MntmFileSaveAs_text;
+	public static String Main_MntmFileSaveNew_text;
+	public static String Main_MntmHelp_text;
+	public static String Main_MntmHelpAbout_text;
+	public static String Main_MntmTools_text;
+	public static String Main_MntmToolsOptions_text;
+	public static String Main_btnSaveNew_text;
+	public static String Main_btnSave_text;
+	public static String Main_btnNew_text;
+	public static String Main_tltmSavenew_text;
+	public static String Main_tltmSave_text;
+	public static String Main_tltmNew_text;
+	public static String Main_lblArchiveSn_text;
+	public static String Main_lblArchiveName_text;
+	public static String Main_lblLinks_text;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
